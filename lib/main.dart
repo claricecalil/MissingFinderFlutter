b5136c9c9889ddf9f77b61937c3454f7297808b9
Clarice Calil <cla.calil@gmail.com>
2020-10-09 15:44:04 -0300

    Missing Finder V1

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//import 'package:missing_finder/Screen/AbrirGaleria.dart';
+import 'package:missing_finder/Screen/AdicionarAnuncio.dart';
+import 'package:missing_finder/Screen/InstrucoesFoto.dart';
+import 'package:missing_finder/Screen/Profile.dart';
+import 'package:missing_finder/Screen/TirarFoto.dart';
+import 'Screen/Home.dart';
+import 'routes/app_routes.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+    routes: {
+      //AppRoutes.HOME: (context) => HomePage(),
+      AppRoutes.ADICIONAR_ANUNCIO: (context) => AdicionarAnuncio(),
+      AppRoutes.PROFILE: (context) => Profile(),
+      AppRoutes.INSTRUCOES_FOTO: (context) => InstrucoesFoto(),
+      AppRoutes.TIRAR_FOTO: (context) => TirarFoto(),
+      //AppRoutes.ABRIR_GALERIA: (context) => AbrirGaleria(),      
+    },
+  ));
+}
